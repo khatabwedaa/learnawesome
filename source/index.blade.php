7038dbb8c94989514f8b955866393894131e92cb
khatabwedaa <khatabwedaa@gmail.com>
2020-05-16 01:08:36 +0200

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,16 +1,8 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
-
-<div x-data="{ open: false }">
-    <button @click="open = true">Open Dropdown</button>
-
-    <ul
-        x-show="open"
-        @click.away="open = false"
-    >
-        Dropdown Body
-    </ul>
+<div class="flex flex-col items-center justify-center h-screen bg-gray-900">
+    <h1 class="text-gray-100 font-semibold text-xl text-center">Tailwindcss Component <br> for learn awesome website</h1>
+    <a class="text-gray-400 mt-4" href="/cards">Cards Page</a>
 </div>
 @endsection
