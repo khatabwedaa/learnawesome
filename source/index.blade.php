d07e05d0b64d6a0119332e3d6ea9d75da747808a
khatabwedaa <khatabwedaa@gmail.com>
2020-05-25 00:15:09 +0200

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,7 +3,8 @@
 @section('body')
 <div class="flex flex-col items-center justify-center h-screen bg-gray-900">
     <h1 class="text-gray-100 font-semibold text-xl text-center">Tailwindcss Component <br> for learn awesome website</h1>
-    <a class="text-gray-400 mt-4" href="/cards">Cards Page</a>
+    <a class="text-gray-400 mt-4" href="/cards">Cards</a>
     <a class="text-gray-400 mt-4" href="/cards-scroll">Cards Scroll</a>
+    <a class="text-gray-400 mt-4" href="/navigation">Navigation</a> 
 </div>
 @endsection
