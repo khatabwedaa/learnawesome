5ea7ab476a81e5a41b984d266e1e653dfa947b56
khatabwedaa <khatabwedaa@gmail.com>
2020-05-24 16:39:28 +0200

    wip

--- source/cards-scroll.blade.php
+++ source/cards-scroll.blade.php
@@ -3,42 +3,42 @@
 @section('body')
     <div class="py-12 px-6">
         <div class="overflow-auto whitespace-no-wrap md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:overflow-hidden md:gap-6">
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
 
-            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3">
+            <div class="inline-block max-w-xs rounded-md overflow-hidden mr-3 md:mr-0">
                 <img class="h-48 w-full object-cover object-center" src="https://images.unsplash.com/photo-1590301175537-e79a7bedf6d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="">
                 <h3 class="bg-gray-800 font-medium text-white py-2 px-4">Gamming</h3>
             </div>
