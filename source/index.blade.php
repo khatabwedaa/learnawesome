9acc684a0ec3e36040e0501d9a83b3ade3b945f1
khatabwedaa <khatabwedaa@gmail.com>
2020-07-24 19:00:51 +0200

    wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,5 +6,6 @@
     <a class="text-gray-400 mt-4" href="/cards">Cards</a>
     <a class="text-gray-400 mt-4" href="/cards-scroll">Cards Scroll</a>
     <a class="text-gray-400 mt-4" href="/navigation">Navigation</a> 
+    <a class="text-gray-400 mt-4" href="/profile">profile</a> 
 </div>
 @endsection
